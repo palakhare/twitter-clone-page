3eaee3b3743bdd1632532e3f2b4a6b05bcbb21b9
palakhare <palakkhare1310@gmail.com>
2026-01-06 16:54:44 +0530

    first commit

--- twitter/next.config.ts
+++ twitter/next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  
   env: {
     BACKEND_URL: process.env.BACKEND_URL,
-
-};
-eslint: {
-ignoreDuringBuilds: true,
-},
+  },
 };
 
 export default nextConfig;
+
