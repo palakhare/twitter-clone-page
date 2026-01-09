90c75b69f7d5fad77ac5ba8d13919b74c98d1701
palakhare <palakkhare1310@gmail.com>
2026-01-09 16:27:48 +0530

    Cleaned next config and optimized env setup

--- twitter/next.config.ts
+++ twitter/next.config.ts
@@ -1,10 +1,7 @@
+
+
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  env: {
-    BACKEND_URL: process.env.BACKEND_URL,
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
-
